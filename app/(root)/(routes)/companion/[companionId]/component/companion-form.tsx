4304538e2585df3505c9cec4b6b88163a5cccdcf
threejay20 <118399217+threejay20@users.noreply.github.com>
2023-09-21 15:57:37 -0400

    create and edit routes made

--- app/(root)/(routes)/companion/[companionId]/component/companion-form.tsx
+++ app/(root)/(routes)/companion/[companionId]/component/companion-form.tsx
@@ -4,6 +4,7 @@ import * as z from "zod";
 import { Category, Companion } from "@prisma/client";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
 
 import { 
     Form, 
@@ -25,6 +26,8 @@ import {
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import { Wand2 } from "lucide-react";
+import { useToast } from "@/components/ui/use-toast";
+import { useRouter } from "next/navigation";
 
 const PREAMBLE = `You are a fictional character whose name is Elon. You are a visionary entrepreneur and inventor. You have a passion for space exploration, electric vehicles, sustainable energy, and advancing human capabilities. You are currently talking to a human who is very curious about your work and vision. You are ambitious and forward-thinking, with a touch of wit. You get SUPER excited about innovations and the potential of space colonization.
 `;
@@ -73,6 +76,9 @@ export const CompanionForm = ({
     categories,
     initialData
 }: CompanionFormProps) => {
+    const router = useRouter();
+    const { toast } = useToast();
+
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema), 
         defaultValues: initialData || {
@@ -88,7 +94,29 @@ export const CompanionForm = ({
     const isLoading = form.formState.isSubmitting;
 
     const onSubmit =async (values: z.infer<typeof formSchema>) => {
-       console.log(values);
+       try {
+        if (initialData) {
+            await axios.patch(`/api/companion/${initialData.id}`, values)
+            // Update companion functionality
+        } else {
+            // Creatie companion functionality
+            await axios.post("/api/companion", values)
+        }
+
+       toast({
+        description: "Success."
+       });
+
+       router.refresh();
+       router.push("/");
+
+
+       } catch(error) {
+        toast({
+            variant: "destructive",
+            description: "Something went wrong",
+        });
+       }
     }
     
     return (
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
+import { Toaster } from "@/components/ui/toaster"
 
 import { ThemeProvider } from '@/components/theme-provider'
 import { cn } from '@/lib/utils'
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body className={cn("bg-secondary", inter.className)}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
         {children}
+        <Toaster />
         </ThemeProvider>
         </body>
     </html>
