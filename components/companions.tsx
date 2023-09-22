a2ad82a1c33f7869ff37d8899b9560caadf4adb0
threejay20 <118399217+threejay20@users.noreply.github.com>
2023-09-22 14:10:30 -0400

    updated category scripts

--- components/companions.tsx
+++ components/companions.tsx
@@ -1,7 +1,8 @@
 import { Companion } from "@prisma/client"
 import Image from "next/image";
-import { Card, CardHeader } from "./ui/card";
+import { Card, CardFooter, CardHeader } from "./ui/card";
 import Link from "next/link";
+import { MessagesSquare } from "lucide-react";
 
 interface CompanionsProps {
     data: (Companion & {
@@ -52,8 +53,27 @@ export const Companions = ({
                         alt="Companion"
                         />
                         </div>
+                        <p className="font-bold">
+                            {item.name}
+                             </p>
+                             <p className="text-xs">
+                                {item.description}
+
+                             </p>
 
                     </CardHeader>
+                    <CardFooter className="flex items-center justify-between text-xs text-muted-foreground">
+                        <p className="lowercase">
+                           @{item.userName} 
+                        </p>
+                        <div className="flex items-center">
+                            <MessagesSquare className="w-3 h-3 mr-1"/>
+                            {item._count.messages}
+
+
+                        </div>
+
+                    </CardFooter>
                 
                 </Link>
 
--- scripts/seed.ts
+++ scripts/seed.ts
@@ -6,13 +6,13 @@ async function main () {
     try{
         await db.category.createMany({
             data: [
-                {name: "Famous People"},
-                {name: "Movies & TV"},
+                {name: "Friends"},
+                {name: "Family"},
                 {name: "Musicians"},
                 {name: "Games"},
-                {name: "Animals"},
-                {name: "Philosophy"},
-                {name: "Scientists"},
+                {name: "Work"},
+                {name: "Movies & TV"},
+                
             ]
         })
 
