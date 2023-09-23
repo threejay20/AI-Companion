e0de983818a9218560c1d2583f826a2bdc39cab0
threejay20 <118399217+threejay20@users.noreply.github.com>
2023-09-23 13:06:14 -0400

    chat header created as well as edit and delete

--- app/api/companion/[companionId]/route.ts
+++ app/api/companion/[companionId]/route.ts
@@ -1,6 +1,6 @@
 import prismadb from "@/lib/prismadb";
 
-import { currentUser } from "@clerk/nextjs";
+import { auth, currentUser } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
 export async function PATCH(
@@ -49,5 +49,30 @@ export async function PATCH(
     }catch(error) {
         console.log("[COMPANION_PATCH]]", error);
         return new NextResponse("internal Error", {status: 500})
+    }
+}
+
+export async function DELETE(
+    request: Request,
+    { params }: {params: { companionId: string}  }
+) {
+    try {
+        const { userId } = auth();
+
+        if (!userId) {
+            return new NextResponse("Unauthorized", { status: 401} );
+        }
+
+        const companion = await prismadb.companion.delete({
+            where: {
+                userId,
+                id: params.companionId,
+            }
+        });
+
+        return NextResponse.json(companion);
+    }catch (error){
+        console.log("[COMPANION_DELETE", error)
+        return new NextResponse("Internal Error", {status:500})
     }
 }
